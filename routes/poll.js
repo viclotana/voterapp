d14f976959d61f74f830d8275e01cb528190406b
viclotana <viclotana@gmail.com>
2018-08-09 19:52:47 +0100

    brought in pusher

--- routes/poll.js
+++ routes/poll.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const pusher = require('pusher');
 
+var pusher = new Pusher({
+    appId: '575181',
+    key: '0af8bb55b80a7fecbf5a',
+    secret: '2fcde39e0afccb216e68',
+    cluster: 'eu',
+    encrypted: true
+  });
 router.get('/', (req, res) => {
     res.send('POLL');
 });
 
 router.post('/', (req,res) =>{
-    
+    pusher.trigger('got-poll', 'got-vote', {
+        
+      });
 })
 module.exports = router;
