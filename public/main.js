237c8ec59cc02ba6bf0fb8f5c1af9a2967524d19
viclotana <viclotana@gmail.com>
2018-08-09 21:17:22 +0100

    upss

--- public/main.js
+++ public/main.js
@@ -1,16 +1,17 @@
 const form = document.getElementById('vote-form');
 form.addEventListener('submit', (event)=>{
-    const choice = document.querySelector
-    ('input[name=got]:checked').value;
+    const choice = document.querySelector('input[name=got]:checked').value;
     const data = {got:choice};
 
-    fetch('http://localhost:3000/poll', {
+    fetch('http://localhost:5000/poll', {
         method:'post',
         body:JSON.stringify(data),
         headers: new Headers({
             'Content-Type':'application/json'
         })
     })
-        .then();
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
     event.preventDefault();
 })
