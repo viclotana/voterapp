402e9b0d41bec86854d87e69297df8fc12107985
viclotana <viclotana@gmail.com>
2018-08-09 21:17:30 +0100

    uppss

--- public/main.js
+++ public/main.js
@@ -15,3 +15,12 @@ form.addEventListener('submit', (event)=>{
         .catch(err => console.log(err));
     event.preventDefault();
 })
+
+let dataPoints = [
+    {label: 'Ramsey Bolton', y:0},
+    {label: 'Sansa Stark', y:0},
+    {label: 'Denarys Tygarian', y:0},
+    {label: 'John Snow', y:0}
+];
+
+const chartContainer = document.querySelector('#chartContainer')
