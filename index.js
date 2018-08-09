38bb79d29e23a18ae9b37988bd22ec0de33ac1b4
viclotana <viclotana@gmail.com>
2018-08-09 19:29:05 +0100

    initialized all dependencies

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//enable cors
+app.use(cors());
+const port = 5000;
+
+//start server
+app.listen(port, () => console.log(`server started on port ${port}`));
