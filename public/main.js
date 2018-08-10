bba245724e1cd7092ef1af1bbaa4011de2178e56
viclotana <viclotana@gmail.com>
2018-08-10 20:24:54 +0100

    done

--- public/main.js
+++ public/main.js
@@ -19,51 +19,58 @@ form.addEventListener('submit', (event)=>{
 fetch('http://localhost:5000/poll')
 .then(res => res.json())
         .then(data =>{
-            console.log(data);
-        })
+            const votes = data.votes;
+            const totalVotes = votes.length;
+            // count vote points -acc/current
+            const voteCounts = votes.reduce(
+                (acc,vote)=> 
+                (acc[vote.got]=
+                ((acc[vote.got] || 0)+ parseInt(vote.points)), acc),{});
 
-let dataPoints = [
-    {label: 'Ramsey Bolton', y:0},
-    {label: 'Sansa Stark', y:0},
-    {label: 'Denarys Tygarian', y:0},
-    {label: 'John Snow', y:0}
-];
+                let dataPoints = [
+                    {label: 'Ramsey', y:voteCounts.Ramsey},
+                    {label: 'Sansa', y:voteCounts.Sansa},
+                    {label: 'Denarys', y:voteCounts.Denarys},
+                    {label: 'John', y:voteCounts.John}
+                ];
+                
+                const chartContainer = document.querySelector('#chartContainer');
+                
+                if (chartContainer) {
+                    const chart = new CanvasJS.Chart('chartContainer', {
+                        animationEnabled: true,
+                        theme: 'theme1',
+                        title:{
+                          text: `Total Votes ${totalVotes}`
+                        },
+                        data:[
+                            {
+                                type:'column',
+                                dataPoints: dataPoints
+                            }
+                        ]
+                    });
+                    chart.render();
+                
+                    // Enable pusher logging - don't include this in production
+                    Pusher.logToConsole = true;
+                
+                    var pusher = new Pusher('0af8bb55b80a7fecbf5a', {
+                      cluster: 'eu',
+                      encrypted: true
+                    });
+                
+                    var channel = pusher.subscribe('got-poll');
+                    channel.bind('got-vote', function(data) {
+                      dataPoints = dataPoints.map(x => {
+                          if (x.label == data.got) {
+                              x.y += data.points;
+                              return x;
+                
+                          } else { return x;}
+                      });
+                      chart.render();
+                    });
+                }
+        });
 
-const chartContainer = document.querySelector('#chartContainer');
-
-if (chartContainer) {
-    const chart = new CanvasJS.Chart('chartContainer', {
-        animationEnabled: true,
-        theme: 'theme1',
-        title:{
-          text: 'GOT Results'
-        },
-        data:[
-            {
-                type:'column',
-                dataPoints: dataPoints
-            }
-        ]
-    });
-    chart.render();
-
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
-
-    var pusher = new Pusher('0af8bb55b80a7fecbf5a', {
-      cluster: 'eu',
-      encrypted: true
-    });
-
-    var channel = pusher.subscribe('got-poll');
-    channel.bind('got-vote', function(data) {
-      dataPoints = dataPoints.map(x => {
-          if (x.label == data.got) {
-              x.y += data.points;
-              return x;
-
-          } else { return x;}
-      });
-      chart.render();
-    });
-}
